d543e3175a4f998ba429b15c9960be297a1e7dc9
devjaeyoon <devjaeyoon@gmail.com>
2025-07-12 23:47:25 +0900

    feat: add SelectItem component for selectable dropdown options
    - Implement SelectItem to render individual options within the Select
      dropdown using context.
    - Displays a check icon for the selected option and highlights on hover.
    - Supports custom className for styling flexibility.

---
+++ src/components/ui/Select/SelectItem.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+import { useSelectContext } from '@/components/ui/Select/context';
+import { cn } from '@/utils/cn';
+
+interface SelectItemProps {
+  children: ReactNode;
+  className?: string;
+  value: string;
+}
+
+export default function SelectItem({ children, className, value }: SelectItemProps) {
+  const { selectedValue, onValueChange } = useSelectContext();
+
+  return (
+    <div
+      className={cn(
+        'relative flex w-full cursor-default items-center rounded-sm border border-transparent py-1.5 pr-2 pl-2 text-sm outline-none select-none hover:border-black hover:bg-gray-200 data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+        className
+      )}
+      onClick={() => onValueChange(value)}
+    >
+      {selectedValue === value ? (
+        <Image
+          src="/check.svg"
+          alt="선택된 옵션 아이콘"
+          width={16}
+          height={16}
+          priority
+          className="mr-2"
+        />
+      ) : (
+        <div className="mr-2 w-4" />
+      )}
+      {children}
+    </div>
+  );
+}
